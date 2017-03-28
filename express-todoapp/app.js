2b19bfd11b311cb3f749ad446584918f1caefeb3
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-03-28 23:28:50 +0200

    editing and updating todos

--- express-todoapp/app.js
+++ express-todoapp/app.js
@@ -8,6 +8,7 @@ const port = 3000;
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const url = 'mongodb://localhost:27017/express-todoapp';
 
 // Boby Parser Middleware
@@ -47,3 +48,60 @@ app.get('/', (req, res, next) => {
     });
 });
 
+app.post('/todo/add', (req, res, next) => {
+    // Create
+    const todo = {
+        title: req.body.title,
+        body: req.body.body
+    }
+
+    // Insert
+    Todos.insert(todo, (err, result) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log('Todo added');
+        res.redirect('/');
+    });
+});
+
+app.delete('/todo/delete/:id', (req, res, next) => {
+    const query = {_id: ObjectID(req.params.id)}
+    Todos.deleteOne(query, (err, response) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log('Todo Removed');
+        res.sendStatus(200);
+    })
+});
+
+app.get('/todo/edit/:id', (req, res, next) => {
+    const query = {_id: ObjectID(req.params.id)}
+    Todos.find(query).next((err, todo) => {
+        if(err){
+            return console.log(err);
+        }
+        res.render('edit', {
+            todo: todo
+        });
+    });
+});
+
+app.post('/todo/edit/:id', (req, res, next) => {
+    const query = {_id: ObjectID(req.params.id)};
+    // Create
+    const todo = {
+        title: req.body.title,
+        body: req.body.body
+    };
+
+    // Update to do
+    Todos.updateOne(query, {$set:todo}, (err, result) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log('Todo updated');
+        res.redirect('/');
+    });
+});

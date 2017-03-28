4307fb2f8f7d1a5ce21ef8cb3a984b1b10d8e4a3
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-03-28 15:49:01 +0200

    contact form

--- repair/app.js
+++ repair/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var about = require('./routes/about');
 var services = require('./routes/services');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/about', about);
 app.use('/services', services);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

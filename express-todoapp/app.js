7d924657b2b2174cca8abf0d8060e94614fb4514
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-03-28 21:22:56 +0200

    mongodb connection

--- express-todoapp/app.js
+++ express-todoapp/app.js
@@ -7,6 +7,9 @@ const port = 3000;
 // Init app
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/express-todoapp';
+
 // Boby Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,12 +19,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('vies', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//noinspection BadExpressionStatementJS
-app.get('/', (req, res, next) => {
-    res.render('index');
+// Connect to mongodb
+
+MongoClient.connect(url, (err, database) => {
+    console.log('MongoDB Connected...');
+    if(err) throw err;
+
+    db = database;
+    Todos = db.collection('todos');
+
+    app.listen(port, () => {
+    console.log('Server running on port ' + port);
+});
+
 });
 
 //noinspection BadExpressionStatementJS
-app.listen(port, () => {
-    console.log('Server running on port ' + port);
+app.get('/', (req, res, next) => {
+    Todos.find({}).toArray((err, todos) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log(todos);
+        res.render('index', {
+            todos: todos
+        });
+    });
 });
+

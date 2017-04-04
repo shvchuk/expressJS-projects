e98b423c1c9e0015225cd2b2858bd87c6b3006a2
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-04 09:47:58 +0200

    routes added

--- sprtblog/routes/categories.js
+++ sprtblog/routes/categories.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('Categories');
+    res.render('categories', {title: 'Categories'});
 });
 
 module.exports = router;

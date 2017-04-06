67a25137465a6bf192384b915aa26f73ee2e9138
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-06 12:51:37 +0200

    passport login system init

---
+++ passport-login/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+

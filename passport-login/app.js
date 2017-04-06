bb2454a745a7e45223c816845a6f10f2af0c3a6f
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-04-06 16:03:24 +0200

    passport login system route & views

--- passport-login/app.js
+++ passport-login/app.js
@@ -8,3 +8,74 @@ const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+const port = 3000;
+
+// Route Files
+const index = require('./routes/index');
+
+// App init
+const app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Express Messages
+app.use(require('connect-flash')());
+app.use((req, res, next) => {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: (param, msg, value) => {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while(namespace.length){
+            formParam +='[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+// Setting Routes
+app.use('/', index);
+
+// Start server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
+
